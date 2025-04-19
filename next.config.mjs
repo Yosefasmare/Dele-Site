7f7e1d1d676c5c77648274893709641dc978cdf7
joseph asmare <160507197+Yosefasmare@users.noreply.github.com>
2025-04-19 11:41:15 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,51 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 's.pinimg.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cloud.appwrite.io',
-      },
-    ],
-  },
-
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self'; object-src 'none'; frame-ancestors 'none';",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "no-referrer-when-downgrade",
-          },
-          {
-            key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=()",
-          },
-        ],
-      },
-    ];
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 's.pinimg.com'
+            },
+            {
+                protocol : 'https',
+                hostname: 'lh3.googleusercontent.com'
+              },
+              {
+                protocol : 'https',
+                hostname: 'cloud.appwrite.io'
+              },
+        ]
+    }
 };
 
 export default nextConfig;
